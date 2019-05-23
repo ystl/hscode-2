cd8cd838a0ffc1bb5cdccb5568f2d217cee685fc
Administrator <1253356807@qq.com>
2019-05-23 14:30:22 +0800

    Initial commit

---
+++ src/main/java/com/td/hscode2/Hscode2Application.java
@@ -0,0 +1,13 @@
+package com.td.hscode2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hscode2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hscode2Application.class, args);
+    }
+
+}
